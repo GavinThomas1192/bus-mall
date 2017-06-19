3ba3c6604e73070a70ad4feb1d9cdf29f11b7ace
GavinThomas1192 <gthomas1192@gmail.com>
2017-06-19 12:16:36 -0700

    setup template

---
+++ app.js
@@ -0,0 +1,9 @@
+'use-strict';
+alert('test');
+var allProducts = [];
+
+function saleItem(name, path, tally, views) {
+  allProducts.push(item);
+};
+
+var chair = new saleItem(chair, img / chair, 2, 34);
